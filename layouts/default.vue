4f6dfd57fe2460b4b0acb6559f51a869f5f17f3d
vBubbaa <itylerjames@gmail.com>
2020-08-23 13:27:36 -0700

    Boilerplate setup for navbar & side drawer + links

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,88 +1,27 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+  <v-app class="app-wrapper">
+    <v-navigation-drawer v-model="drawer" fixed app temporary color="#333333"></v-navigation-drawer>
+    <!-- Main navbar -->
+    <v-app-bar color="transparent" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+      <nuxt-link to="/" class="default-link">
+        <v-toolbar-title v-text="title" />
+      </nuxt-link>
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
+      <v-btn text>
+        <nuxt-link to="/games">Games</nuxt-link>
+      </v-btn>
+      <v-btn text>
+        <nuxt-link to="/about">About</nuxt-link>
       </v-btn>
     </v-app-bar>
+    <!-- Main content -->
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+    <v-footer :absolute="!fixed" app color="#333333">
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -90,28 +29,38 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      clipped: false,
       drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
-}
+      fixed: true,
+      title: "Steam Comparer",
+    };
+  },
+};
 </script>
+
+<style scoped>
+.app-wrapper {
+  background-color: #141414;
+}
+</style>
+
+// Global styles
+<style>
+a {
+  color: #e0e1dd !important;
+  text-decoration: none !important;
+}
+
+.nuxt-link-active {
+  color: #ed254e !important;
+  font-weight: bold !important;
+}
+
+.default-link {
+  color: #e0e1dd !important;
+  text-decoration: none !important;
+}
+</style>
+  
+</style>
