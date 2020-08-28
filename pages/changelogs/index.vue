5889db3011d23bb7afefba1844b3f23d43ebe480
vBubbaa <itylerjames@gmail.com>
2020-08-28 16:53:44 -0700

    Changelogs setup w/ pagination, searching, loading

--- pages/changelogs/index.vue
+++ pages/changelogs/index.vue
@@ -1,10 +1,85 @@
 <template>
-  <div>changelogs index</div>
+  <div>
+    <v-container v-if="$fetchState.pending">
+      <v-row justify="center" class="text-center">
+        <v-col cols="12">
+          <v-progress-circular :size="70" :width="7" color="#ed254e" indeterminate></v-progress-circular>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container v-else>
+      <v-row>
+        <v-col cols="12">
+          <v-text-field v-model="payload.search" @input="setSearchQuery()" color="#e0e1dd">
+            <template v-slot:label>
+              Search for change logs
+              <v-icon style="vertical-align: middle">mdi-magnify</v-icon>
+            </template>
+          </v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <ChangeLogs :logs="logs.data" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-pagination
+            color="#ed254e"
+            v-model="logs.pagination.current_page"
+            :length="logs.pagination.num_pages"
+            :total-visible="8"
+            @input="nextPage"
+          ></v-pagination>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
-export default {};
+import ChangeLogs from "../../components/games/ChangeLogs";
+import mapState from "vuex";
+
+export default {
+  name: "ChangeLogsListView",
+  components: {
+    ChangeLogs,
+  },
+  data() {
+    return {
+      logs: [],
+      payload: {
+        page: "",
+        search: "",
+      },
+    };
+  },
+  methods: {
+    nextPage(page) {
+      this.payload.page = page;
+      this.$fetch();
+    },
+    setSearchQuery: function () {
+      this.payload.page = 1;
+      this.$fetch();
+    },
+  },
+
+  async fetch() {
+    if (this.payload.page == "") {
+      this.logs = await this.$axios.$get("/api/alllogs/", {
+        params: { page: 1, search: this.payload.search },
+      });
+    } else {
+      this.logs = await this.$axios.$get("/api/alllogs/", {
+        params: this.payload,
+      });
+    }
+  },
+};
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 </style>
