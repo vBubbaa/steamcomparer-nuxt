43a30e7e12f218bfa2958a44b164d85d9505570b
vBubbaa <itylerjames@gmail.com>
2020-09-13 12:23:30 -0700

    Add favicon + variables.scss file

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/sc-icon-3.png" }]
   },
   /*
    ** Global CSS
